3e93390d502965028c341e6c33e8127908c2fd91
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2018-12-27 11:42:05 -0500

    Update ReactBnbGallery.js

--- src/ReactBnbGallery.js
+++ src/ReactBnbGallery.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { omit } from 'lodash';
+import omit from 'lodash/omit';
 import PropTypes from 'prop-types';
 import FocusTrap from 'focus-trap-react';
 import { Portal } from 'react-portal';
