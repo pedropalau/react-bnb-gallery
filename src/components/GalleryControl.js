16f860430cdeefcd95c55436b66d1ba4b85deeca
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-01-04 12:58:35 -0500

    Update GalleryControl.js

--- src/components/GalleryControl.js
+++ src/components/GalleryControl.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import classnames from 'classnames';
-import { noop } from '../utils/functions';
+
+import noop from '../utils/noop';
 
 const svgAttributes = {
   viewBox: '0 0 18 18',
