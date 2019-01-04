062b60e3b6227dbe1c30519004a8d152ce23fb22
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-01-04 12:59:16 -0500

    Create defaultPhrases.js

---
+++ src/defaultPhrases.js
@@ -0,0 +1,9 @@
+const noPhotosProvided = 'No photos to show';
+const showPhotoList = 'Show photo list';
+const hidePhotoList = 'Hide photo list';
+
+export default {
+  noPhotosProvided,
+  showPhotoList,
+  hidePhotoList,
+};
