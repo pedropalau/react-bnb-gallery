4cbef5f9c6ebf0a3d58bdd895780802254cd11e3
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-01-04 12:52:33 -0500

    Allow to use an array of strings

--- src/shapes/PhotosShape.js
+++ src/shapes/PhotosShape.js
@@ -1,11 +1,14 @@
 import PropTypes from 'prop-types';
 
-export default PropTypes.arrayOf(
-  PropTypes.shape({
-    photo: PropTypes.string.isRequired,
-    number: PropTypes.number,
-    caption: PropTypes.string,
-    subcaption: PropTypes.string,
-    thumbnail: PropTypes.string,
-  })
-);
+export default PropTypes.oneOfType([
+  PropTypes.arrayOf(PropTypes.string.isRequired),
+  PropTypes.arrayOf(
+    PropTypes.shape({
+      photo: PropTypes.string.isRequired,
+      number: PropTypes.number,
+      caption: PropTypes.string,
+      subcaption: PropTypes.string,
+      thumbnail: PropTypes.string,
+    })
+  )
+]);
