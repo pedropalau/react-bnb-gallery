50013957ad2027d1d15fd1bf05d73ce1bd4c506e
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-01-04 12:58:39 -0500

    Update GalleryNextButton.js

--- src/components/GalleryNextButton.js
+++ src/components/GalleryNextButton.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import GalleryControl from './GalleryControl';
-import { noop } from '../utils/functions';
+
+import noop from '../utils/noop';
 
 const NEXT_ARROW = 'm4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z';
 
