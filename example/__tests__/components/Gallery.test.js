a151aa070df94d2e12b1b09ce8b567c49a0f7ee7
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-01-18 00:23:49 -0500

    jest tests configured

---
+++ example/__tests__/components/Gallery.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Gallery from '../../../src/components/Gallery';
+import GalleryCaption from '../../../src/components/GalleryCaption';
+
+import photos from '../test-photos';
+
+const requiredProps = {
+  photos,
+}
+
+describe('Gallery', () => {
+  describe('#render', () => {
+    it('renders <GalleryCaption />', () => {
+      const wrapper = shallow((
+        <Gallery
+          {...requiredProps}
+          showThumbnails={true}
+        />
+      ));
+      expect(wrapper.find(GalleryCaption)).toHaveLength(1);
+    });
+    it('<GalleryCaption /> is not rendered if props.showThumbnails === false', () => {
+      const wrapper = shallow((
+        <Gallery
+          {...requiredProps}
+          showThumbnails={false}
+        />
+      ));
+      expect(wrapper.find(GalleryCaption)).toHaveLength(0);
+    });
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  verbose: true,
+  /*collectCoverage: true,
+  collectCoverageFrom : [
+    "src/*.{js,jsx}"
+  ],*/
+  testMatch: [
+    "**/__tests__/**/*.test.js?(x)"
+  ],
+  setupFiles: [
+    "./example/__tests__/jestSetup.js"
+  ],
+  moduleNameMapper: {
+     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+     "\\.(css|less)$": "identity-obj-proxy"
+   }
+};
