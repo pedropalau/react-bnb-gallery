5ebbb71f4f960abe200d7cf872e967877855f041
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-01-18 00:22:20 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -10,6 +10,8 @@ export const BACKWARDS = 'backwards';
 export const ARROW_LEFT_KEYCODE = 37;
 export const ARROW_RIGHT_KEYCODE = 39;
 export const ESC_KEYCODE = 27;
+export const ENTER_KEYCODE = 13;
+export const SPACE_KEYCODE = 32;
 
 export const TOUCHEVENT_COMPAT_WAIT = 500;
 export const SWIPE_THRESHOLD = 40;
@@ -17,3 +19,11 @@ export const SWIPE_THRESHOLD = 40;
 export const THUMBNAIL_WIDTH = 100;
 export const THUMBNAIL_HEIGHT = 67;
 export const THUMBNAIL_OFFSET = 10;
+
+export const defaultPhotoProps = {
+  photo: undefined,
+  number: undefined,
+  caption: undefined,
+  subcaption: undefined,
+  thumbnail: undefined,
+};
