8d09425fb8b5edda896d672f2dbbbd12a4386f21
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-06 21:32:00 -0500

    Update index.js

--- example/src/sections/License/index.js
+++ example/src/sections/License/index.js
@@ -4,7 +4,7 @@ import Container from '../../components/Container';
 import Text from '../../components/Text';
 
 const License = () => (
-  <Container style={{ textAlign: 'left' }}>
+  <Container className="container" style={{ textAlign: 'left' }}>
     <Text inherit><strong>React Photo Gallery</strong> is free to use for personal and commercial projects under <a href="https://github.com/peterpalau/react-bnb-gallery/blob/master/LICENSE" target="_blank" rel="noopener noreferrer">the MIT license</a>.</Text>
     <Text inherit>Attribution is not required, but greatly appreciated. It does not have to be user-facing and can remain within the code.</Text>
   </Container>
