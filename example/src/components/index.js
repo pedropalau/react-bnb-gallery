2086849f5d390ab59d942af579d19b8d8271d709
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-06 21:29:49 -0500

    Export Spacing component

--- example/src/components/index.js
+++ example/src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Action } from './Anchor';
 export { default as Button } from './Button';
 export { default as Container } from './Container';
+export { default as Spacing } from './Spacing';
 export { default as Text } from './Text';
 export { default as Title } from './Title';
 export { default as PhotoGrid } from './PhotoGrid';
