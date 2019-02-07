368858c27112849845f44f1bc4b169e3db72f5a9
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-06 21:32:25 -0500

    Create galleryDefaultProps.js

---
+++ src/common/galleryDefaultProps.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import noop from '../utils/noop';
+import defaultPhrases from '../defaultPhrases';
+
+import { FORWARDS } from '../constants';
+
+export default {
+  activePhotoIndex: 0,
+  activePhotoPressed: noop,
+  direction: FORWARDS,
+  nextButtonPressed: noop,
+  prevButtonPressed: noop,
+  showThumbnails: true,
+  photos: [],
+  preloadSize: 5,
+  wrap: false,
+  phrases: defaultPhrases,
+};
