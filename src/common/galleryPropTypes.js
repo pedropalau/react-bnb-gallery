28a79909fe67d293b5977de6d6ce86031f3fbd95
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-06 21:32:28 -0500

    Create galleryPropTypes.js

---
+++ src/common/galleryPropTypes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  forbidExtraProps,
+  nonNegativeInteger,
+} from './prop-types';
+
+import SlideDirectionShape from '../shapes/SlideDirectionShape';
+import PhotosShape from '../shapes/PhotosShape';
+
+import defaultPhrases from '../defaultPhrases';
+import getPhrasePropTypes from '../utils/getPhrasePropTypes';
+
+export default forbidExtraProps({
+  activePhotoIndex: nonNegativeInteger,
+  activePhotoPressed: PropTypes.func,
+  direction: SlideDirectionShape,
+  nextButtonPressed: PropTypes.func,
+  prevButtonPressed: PropTypes.func,
+  showThumbnails: PropTypes.bool,
+  photos: PhotosShape,
+  preloadSize: nonNegativeInteger,
+  wrap: PropTypes.bool,
+  phrases: PropTypes.shape(getPhrasePropTypes(defaultPhrases)),
+});
