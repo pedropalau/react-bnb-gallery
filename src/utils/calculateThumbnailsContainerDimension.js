22c1b4cb946e9e8d95af4cf279d7ffaa1321e764
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:58:14 -0500

    Update calculateThumbnailsContainerDimension.js

--- src/utils/calculateThumbnailsContainerDimension.js
+++ src/utils/calculateThumbnailsContainerDimension.js
@@ -1,4 +1,7 @@
-import { THUMBNAIL_WIDTH, THUMBNAIL_OFFSET } from '../constants';
+import {
+  THUMBNAIL_WIDTH,
+  THUMBNAIL_OFFSET,
+} from '../constants';
 
 export default function calculateThumbnailsContainerDimension(total) {
   return (THUMBNAIL_WIDTH * total) + ((THUMBNAIL_OFFSET * total) - THUMBNAIL_OFFSET);
