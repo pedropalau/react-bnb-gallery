9d5d7adfc17905c5828f0edea19f444035037e85
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:59:56 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   verbose: true,
-  /*collectCoverage: true,
+  collectCoverage: true,
   collectCoverageFrom : [
     "src/*.{js,jsx}"
-  ],*/
+  ],
   testMatch: [
-    "**/__tests__/**/*.test.js?(x)"
+    "**/test/**/*.test.js?(x)"
   ],
   setupFiles: [
-    "./example/__tests__/jestSetup.js"
+    "./test/setup.js"
   ],
   moduleNameMapper: {
      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
