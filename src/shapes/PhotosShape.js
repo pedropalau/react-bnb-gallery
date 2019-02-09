9f998f6ef3d69c126c045bd79deabc8d96f40dd3
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:58:32 -0500

    Update PhotosShape.js

--- src/shapes/PhotosShape.js
+++ src/shapes/PhotosShape.js
@@ -1,14 +1,8 @@
 import PropTypes from 'prop-types';
 
+import PhotoShape from './PhotoShape';
+
 export default PropTypes.oneOfType([
   PropTypes.arrayOf(PropTypes.string.isRequired),
-  PropTypes.arrayOf(
-    PropTypes.shape({
-      photo: PropTypes.string.isRequired,
-      number: PropTypes.number,
-      caption: PropTypes.string,
-      subcaption: PropTypes.string,
-      thumbnail: PropTypes.string,
-    }),
-  ),
+  PropTypes.arrayOf(PhotoShape),
 ]);
