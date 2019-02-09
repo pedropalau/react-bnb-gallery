e70647525bdf040d171ab6058b7f931c07b5d090
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:58:58 -0500

    Create GalleryThumbnail.js

---
+++ src/components/GalleryThumbnail.js
@@ -0,0 +1,76 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import classnames from 'classnames';
+
+import Image from './Image';
+import PhotoShape from '../shapes/PhotoShape';
+
+import {
+  forbidExtraProps,
+  nonNegativeInteger,
+} from '../common/prop-types';
+import noop from '../utils/noop';
+
+import {
+  THUMBNAIL_WIDTH,
+  THUMBNAIL_HEIGHT,
+} from '../constants';
+
+const thumbnailStyle = {
+  width: THUMBNAIL_WIDTH,
+  height: THUMBNAIL_HEIGHT,
+};
+
+const propTypes = forbidExtraProps({
+  active: PropTypes.bool,
+  photo: PhotoShape,
+  onPress: PropTypes.func,
+  number: nonNegativeInteger,
+});
+
+const defaultProps = {
+  active: false,
+  photo: null,
+  onPress: noop,
+  number: 0,
+};
+
+class GalleryThumbnail extends PureComponent {
+  render() {
+    const {
+      active,
+      photo,
+      onPress,
+      number,
+    } = this.props;
+
+    const className = classnames(
+      'thumbnail-button',
+      active && 'active',
+    );
+
+    return (
+      <button
+        type="button"
+        aria-label={photo.caption}
+        className={className}
+        data-photo-index={number}
+        onClick={onPress}
+        disabled={false}
+      >
+        <Image
+          alt={photo.caption}
+          src={photo.thumbnail || photo.photo}
+          className="thumbnail"
+          style={thumbnailStyle}
+        />
+      </button>
+    );
+  }
+}
+
+GalleryThumbnail.propTypes = propTypes;
+GalleryThumbnail.defaultProps = defaultProps;
+
+export default GalleryThumbnail;
