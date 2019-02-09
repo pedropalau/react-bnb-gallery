f26e2973f47f98eb68fd5d0acaf756381a63f31f
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:58:47 -0500

    Update LoadingSpinner.js

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const propTypes = {
+import { forbidExtraProps } from '../common/prop-types';
+
+const propTypes = forbidExtraProps({
   show: PropTypes.bool,
-};
+});
 
 const defaultProps = {
   show: true,
