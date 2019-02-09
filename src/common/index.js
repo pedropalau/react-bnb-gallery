9dfe9dbac4334d0e05de974ffb4c2d97577b253d
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:59:24 -0500

    Update index.js

--- src/common/index.js
+++ src/common/index.js
@@ -1,2 +1,4 @@
-export { default as galleryDefaultProps } from './galleryDefaultProps';
 export { default as galleryPropTypes } from './galleryPropTypes';
+export { default as galleryDefaultProps } from './galleryDefaultProps';
+export { default as imagePropTypes } from './imagePropTypes';
+export { default as imageDefaultProps } from './imageDefaultProps';
