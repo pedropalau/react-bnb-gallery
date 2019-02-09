833d4b83993900f850f659aeb1f5931bf176df3a
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:56:30 -0500

    Create ReactBnbGallery.test.js

---
+++ test/components/ReactBnbGallery.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import ReactBnbGallery from '../../src/ReactBnbGallery';
+import Gallery from '../../src/components/Gallery';
+
+import photos from '../test-photos';
+
+const requiredProps = {
+  photos,
+}
+
+describe('ReactBnbGallery', () => {
+  describe('#render', () => {
+    it('unmounts', () => {
+      const wrapper = mount((
+        <ReactBnbGallery
+          {...requiredProps}
+        />
+      ));
+      wrapper.setProps({ show: true });
+      wrapper.unmount();
+    });
+    it('renders <Gallery />', () => {
+      const wrapper = shallow((
+        <ReactBnbGallery
+          {...requiredProps}
+          show={true}
+        />
+      )).dive();
+      expect(wrapper.find(Gallery)).toHaveLength(1);
+    });
+  });
+});
