f9110cb1dd3ee5ebe5d2e378645e34d08732ff17
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:59:19 -0500

    Create imagePropTypes.js

---
+++ src/common/imagePropTypes.js
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types';
+
+import { forbidExtraProps } from './prop-types';
+
+export default forbidExtraProps({
+  onLoad: PropTypes.func,
+  onError: PropTypes.func,
+});
