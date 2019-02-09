ac62d592888d1b4e92ca8b457c563b4ce33d13bc
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:59:05 -0500

    Update GalleryPrevButton.js

--- src/components/GalleryPrevButton.js
+++ src/components/GalleryPrevButton.js
@@ -3,18 +3,21 @@ import PropTypes from 'prop-types';
 
 import GalleryControl from './GalleryControl';
 
+import { forbidExtraProps } from '../common/prop-types';
 import noop from '../utils/noop';
 
 const PREV_ARROW = 'm13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z';
 
-const propTypes = {
+const propTypes = forbidExtraProps({
   onPress: PropTypes.func,
   disabled: PropTypes.bool,
-};
+  light: PropTypes.bool,
+});
 
 const defaultProps = {
   onPress: noop,
   disabled: false,
+  light: false,
 };
 
 class GalleryPrevButton extends PureComponent {
@@ -22,14 +25,16 @@ class GalleryPrevButton extends PureComponent {
     const {
       onPress,
       disabled,
+      light,
     } = this.props;
-    
+
     return (
       <GalleryControl
         className="gallery-control--prev"
         onPress={onPress}
         arrow={PREV_ARROW}
         disabled={disabled}
+        light={light}
       />
     );
   }
