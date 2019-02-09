4a20d699ef671761e7765a884bdf5a6c45bcac3e
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-09 00:58:35 -0500

    Create PhotoShape.js

---
+++ src/shapes/PhotoShape.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export default PropTypes.shape({
+  photo: PropTypes.string.isRequired,
+  number: PropTypes.number,
+  caption: PropTypes.string,
+  subcaption: PropTypes.string,
+  thumbnail: PropTypes.string,
+});
