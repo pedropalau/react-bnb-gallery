0e3f6d3485afe541f0c89377dc9986538070e41f
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-12 13:51:03 -0500

    Update setup.js
    Setup according to the official Enzyme documentation.

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,4 @@
-import { configure } from 'enzyme';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter()  });
+Enzyme.configure({ adapter: new Adapter()  });
