e3138dc7eda3b67748612a4ed4fc6a05060f61b2
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-11 22:06:36 -0500

    test folder renamed to tests

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
     "src/*.{js,jsx}"
   ],
   testMatch: [
-    "**/test/**/*.test.js?(x)"
+    "**/tests/**/*.test.js?(x)"
   ],
   setupFiles: [
-    "./test/setup.js"
+    "./tests/setup.js"
   ],
   moduleNameMapper: {
      "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
