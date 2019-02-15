adf8a56df5de62c91eabeb19df8f72a8cec6b9c8
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2019-02-15 15:39:20 -0500

    Update setup.js

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter()  });
+import 'jest-enzyme';
+
+Enzyme.configure({ adapter: new Adapter() });
