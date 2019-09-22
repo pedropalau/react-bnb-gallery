8e04707259984324f7dcba1f5c57678ce11c6fbc
Pedro Enrique Palau <pepalauisaac@gmail.com>
2019-09-21 23:28:26 -0500

    Allow to add a class name to Text component

--- example/src/components/Text/index.js
+++ example/src/components/Text/index.js
@@ -5,19 +5,22 @@ import { forbidExtraProps } from 'airbnb-prop-types';
 import classnames from 'classnames';
 
 const propTypes = forbidExtraProps({
+  className: PropTypes.string,
   children: PropTypes.node.isRequired,
   inherit: PropTypes.bool,
 });
 
 const defaultProps = {
+  className: null,
   inherit: false,
 };
 
 const Text = ({
+  className,
   children,
   inherit,
 }) => (
-  <p className={classnames('text', inherit && 'text__inherit')}>
+  <p className={classnames('text', inherit && 'text__inherit', className)}>
     {children}
   </p>
 );
