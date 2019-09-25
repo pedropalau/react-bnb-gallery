224f31bc0681cc7b87be268c5f484dabfcbcf8a3
Pedro Enrique Palau <pepalauisaac@gmail.com>
2019-09-25 16:09:29 -0500

    Export Gallery as an apart component
    Related to #18
    
    This will cause the following update:
    
    `import ReactBnbGallery` from 'react-bnb-gallery'
    
    is now:
    
    `import { ReactBnbGallery } from 'react-bnb-gallery'`
    
    Also `Gallery` is available as an apart component:
    
    `import { Gallery } from 'react-bnb-gallery'`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-import ReactBnbGallery from './ReactBnbGallery';
-
-export default ReactBnbGallery;
+export { default as ReactBnbGallery } from './ReactBnbGallery';
+export { default as Gallery } from './components/Gallery';
