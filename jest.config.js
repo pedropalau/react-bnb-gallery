8927ddbaaa80ba215d0b02da19b6d187c842008f
Pedro Enrique Palau <pepalauisaac@gmail.com>
2019-10-04 13:09:59 -0500

    Add Jest support for SCSS

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,6 @@ module.exports = {
   modulePaths: ["<rootDir>/src","<rootDir>/node_modules"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|less)$": "identity-obj-proxy"
+    "\\.(css|less|scss)$": "identity-obj-proxy"
   }
 };
