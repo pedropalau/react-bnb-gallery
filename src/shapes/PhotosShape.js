1d313df2cf77c59c32130c79096f1a5905d97484
Pedro Palau <pepalauisaac@gmail.com>
2020-06-22 14:50:19 -0500

    Updates photo component shape

--- src/shapes/PhotosShape.js
+++ src/shapes/PhotosShape.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import PhotoShape from './PhotoShape';
 
 export default PropTypes.oneOfType([
+  PropTypes.string.isRequired,
   PropTypes.arrayOf(PropTypes.string.isRequired),
   PropTypes.arrayOf(PhotoShape),
 ]);
