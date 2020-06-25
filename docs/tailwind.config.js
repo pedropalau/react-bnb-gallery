7f21618452fcee3f1133f74967161f10a5fa1038
Pedro Palau <pepalauisaac@gmail.com>
2020-06-25 15:01:41 -0500

    First commit of the new documentation app using Next.js and TailwindCSS

---
+++ docs/tailwind.config.js
@@ -0,0 +1,25 @@
+const tailwindui = require('@tailwindcss/ui');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: [
+    './components/**/*.js',
+    './pages/**/*.js',
+    './pages/**/*.mdx',
+    './jobs/**/*.md',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter var',
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    tailwindui,
+  ],
+};
