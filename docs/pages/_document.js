8db5cc1f5b610053004f9a685e41dd0611a4f312
Pedro Palau <pepalauisaac@gmail.com>
2020-06-25 15:38:38 -0500

    Creates custom document

---
+++ docs/pages/_document.js
@@ -0,0 +1,32 @@
+/* eslint-disable class-methods-use-this */
+import React from 'react';
+
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body className="flex flex-col min-h-screen">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
