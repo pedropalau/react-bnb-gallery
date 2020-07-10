969bd93d39245ab3037d395e6ede38b19529ee2a
Pedro Palau <pepalauisaac@gmail.com>
2020-07-10 16:17:22 -0500

    Updates favicons and adds other metatags

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -23,8 +23,13 @@ const App = ({
 }) => (
   <>
     <Head>
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
     <Component
       {...pageProps}
