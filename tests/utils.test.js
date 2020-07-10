cbaba0b57c480c6075f29b5892b286446b17f1eb
Pedro Palau <pepalauisaac@gmail.com>
2020-07-10 16:04:05 -0500

    Updates tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -13,13 +13,13 @@ describe('the calculateThumbnailsOffset function', () => {
     expect(calculateThumbnailsOffset(0, { width: 100 })).toBe(21);
   });
   it('offset with current 1 and width of 100', () => {
-    expect(calculateThumbnailsOffset(1, { width: 100 })).toBe(-47);
+    expect(calculateThumbnailsOffset(1, { width: 100 })).toBe(-45);
   });
   it('offset with current 1 and width of 175', () => {
-    expect(calculateThumbnailsOffset(1, { width: 175 })).toBe(-9.5);
+    expect(calculateThumbnailsOffset(1, { width: 175 })).toBe(-7.5);
   });
   it('offset with current 100 and width of 999999', () => {
-    expect(calculateThumbnailsOffset(100, { width: 999999 })).toBe(493170.5);
+    expect(calculateThumbnailsOffset(100, { width: 999999 })).toBe(493370.5);
   });
 });
 
@@ -28,12 +28,12 @@ describe('the calculateThumbnailsContainerDimension function', () => {
     expect(calculateThumbnailsContainerDimension(1)).toBe(THUMBNAIL_WIDTH);
   });
   it('dimension with total of 10', () => {
-    expect(calculateThumbnailsContainerDimension(10)).toBe(670);
+    expect(calculateThumbnailsContainerDimension(10)).toBe(652);
   });
   it('dimension with total of 21', () => {
-    expect(calculateThumbnailsContainerDimension(21)).toBe(1418);
+    expect(calculateThumbnailsContainerDimension(21)).toBe(1378);
   });
   it('dimension with total of 999', () => {
-    expect(calculateThumbnailsContainerDimension(999)).toBe(67922);
+    expect(calculateThumbnailsContainerDimension(999)).toBe(65926);
   });
 });
