ef5c08fd82336799d07a90b2db35fe164df78b3f
Pedro Palau <pepalauisaac@gmail.com>
2020-07-12 18:45:38 -0500

    Updates the documentation site header component

--- docs/components/Header/Header.js
+++ docs/components/Header/Header.js
@@ -1,3 +1,19 @@
-const Header = () => null;
+import React from 'react';
+
+import Link from 'next/link';
+
+import Logo from '../Logo';
+
+const Header = () => (
+  <header className="w-full relative py-6 border-b border-gray-200">
+    <div className="container max-w-screen-lg mx-auto px-10">
+      <Link href="/">
+        <a title="Home" className="block w-48">
+          <Logo />
+        </a>
+      </Link>
+    </div>
+  </header>
+);
 
 export default Header;
