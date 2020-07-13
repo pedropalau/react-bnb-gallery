2ae45afbc222c3f3aaeb0343ab41da572095d7ed
Pedro Palau <pepalauisaac@gmail.com>
2020-07-13 10:20:15 -0500

    Uses custom plugin to transpile local packages

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,9 +1,12 @@
 const withMdxEnhanced = require('next-mdx-enhanced');
 const withMDX = require('@next/mdx');
+const withTM = require('next-transpile-modules');
 
 module.exports = withMdxEnhanced({
   layoutPath: 'layouts',
   extendFrontMatter: {
     phase: 'prebuild|loader|both',
   },
-})(withMDX());
+})(withMDX(withTM({
+  transpileModules: ['react-bnb-gallery'],
+})));
