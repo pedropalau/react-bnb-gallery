7e9e59c4f058e62cc91efc0ed2d56f38bf07a677
Pedro Palau <pepalauisaac@gmail.com>
2020-07-15 12:04:01 -0500

    Updates documentation layout component

--- docs/layouts/docs-page.js
+++ docs/layouts/docs-page.js
@@ -1,48 +1,47 @@
 import React from 'react';
 
 import Head from '../components/Head';
-import Layout from '../components/Layout';
-import Sidebar from '../components/Sidebar';
-
-import docsPages from '../data/getDocsPages';
+import Layout, { LayoutContextConsumer } from '../components/Layout';
+import SidebarDocs from '../components/SidebarDocs';
 
 const Doc = ({
   title,
   __resourcePath,
-}) => (
-  ({ children: content }) => (
-    <Layout>
-      <Head title={title} />
-      <div className="container max-w-screen-lg mx-auto px-6 md:px-10">
-        <div className="flex flex-col lg:flex-row -mx-6">
-          <div className="hidden fixed inset-0 pt-16 h-full bg-white z-90 w-full border-b -mb-16 lg:-mb-0 lg:static lg:h-auto lg:overflow-y-visible lg:border-b-0 lg:pt-0 lg:w-1/3 lg:block lg:border-0">
-            <Sidebar menu={docsPages} />
-          </div>
-          <div className="py-10 w-full px-6 md:px-8 lg:px-0 lg:w-2/3">
-            <h1 className="text-2xl lg:text-3xl font-medium flex items-center">
+}) => ({ children: content }) => (
+  <Layout showHeaderFixed>
+    <Head title={title} />
+    <div className="container max-w-screen-lg mx-auto px-6 md:px-10">
+      <div className="flex flex-col lg:flex-row -mx-6">
+        <LayoutContextConsumer>
+          {({ navigationOpened }) => (
+            <SidebarDocs isOpen={navigationOpened} />
+          )}
+        </LayoutContextConsumer>
+        <div className="py-10 w-full px-6 md:px-8 pt-24 lg:pt-28 lg:px-0 lg:w-2/3">
+          <div className="border-b border-gray-200 pb-8 mb-8">
+            <h1 className="text-3xl font-semibold flex items-center">
               {title}
             </h1>
-            <hr className="my-8 border-b border-gray-200" />
-            <div className="prose">
-              {content}
-            </div>
-            <footer className="border-t border-gray-200 mt-10 md:mt-4 pt-4 lg:mt-10 lg:pt-6">
-              <div className="flex">
-                <a
-                  className="text-green-500 underline"
-                  href={`https://github.com/peterpalau/react-bnb-gallery/tree/master/docs/pages/${__resourcePath}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Edit this page on GitHub
-                </a>
-              </div>
-            </footer>
           </div>
+          <div className="prose">
+            {content}
+          </div>
+          <footer className="border-t border-gray-200 mt-10 md:mt-4 pt-4 lg:mt-10 lg:pt-6">
+            <div className="flex">
+              <a
+                className="text-green-500 underline"
+                href={`https://github.com/peterpalau/react-bnb-gallery/tree/master/docs/pages/${__resourcePath}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Edit this page on GitHub
+              </a>
+            </div>
+          </footer>
         </div>
       </div>
-    </Layout>
-  )
+    </div>
+  </Layout>
 );
 
 export default Doc;
