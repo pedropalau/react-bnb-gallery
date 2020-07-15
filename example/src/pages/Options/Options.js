6b4f9f9b6446e5070a3fa350748702ce68a474ff
Pedro Palau <pepalauisaac@gmail.com>
2020-07-15 08:25:58 -0500

    Fixes classnames attribute

--- example/src/pages/Options/Options.js
+++ example/src/pages/Options/Options.js
@@ -27,103 +27,103 @@ const Options = () => (
             <td align="left">
               <code className="prop">activePhotoIndex</code>
             </td>
-            <td align="left"><code class="data-type">number</code></td>
+            <td align="left"><code className="data-type">number</code></td>
             <td align="left"><code>0</code></td>
             <td align="left">Initial photo index to show.</td>
           </tr>
           <tr>
             <td align="left"><code>activePhotoPressed</code></td>
-            <td align="left"><code class="data-type">function</code></td>
+            <td align="left"><code className="data-type">function</code></td>
             <td align="left"><code>noop</code></td>
             <td align="left">Called when a photo is pressed.</td>
           </tr>
           <tr>
             <td align="left"><code>leftKeyPressed</code></td>
-            <td align="left"><code class="data-type">function</code></td>
+            <td align="left"><code className="data-type">function</code></td>
             <td align="left"><code>noop</code></td>
             <td align="left">Called when <code>left</code> key of the keyboard is pressed.</td>
           </tr>
           <tr>
             <td align="left"><code>nextButtonPressed</code></td>
-            <td align="left"><code class="data-type">function</code></td>
+            <td align="left"><code className="data-type">function</code></td>
             <td align="left"><code>noop</code></td>
             <td align="left">Called when <code>next</code> control button is pressed.</td>
           </tr>
           <tr>
             <td align="left"><code>onClose</code></td>
-            <td align="left"><code class="data-type">function</code></td>
+            <td align="left"><code className="data-type">function</code></td>
             <td align="left"><code>noop</code></td>
             <td align="left">Called when the gallery modal is going to close.</td>
           </tr>
           <tr>
             <td align="left"><code>preloadSize</code></td>
-            <td align="left"><code class="data-type">number</code></td>
+            <td align="left"><code className="data-type">number</code></td>
             <td align="left"><code>5</code></td>
             <td align="left">The number of photos to preload on gallery initialization.</td>
           </tr>
           <tr>
             <td align="left"><code>prevButtonPressed</code></td>
-            <td align="left"><code class="data-type">function</code></td>
+            <td align="left"><code className="data-type">function</code></td>
             <td align="left"><code>noop</code></td>
             <td align="left">Called when <code>previous</code> control button is pressed.</td>
           </tr>
           <tr>
             <td align="left"><code>photos</code></td>
-            <td align="left"><code class="data-type">array</code></td>
+            <td align="left"><code className="data-type">array</code></td>
             <td align="left"><code>[]</code></td>
             <td align="left">Array of photos. It can be an <code>array</code> of photos <code>URLs</code> or an <code>array</code> of <code>objects</code>. See the <code>[photo object]</code> props <a href="#photos-properties">bellow</a>.</td>
           </tr>
           <tr>
             <td align="left"><code>phrases</code></td>
-            <td align="left"><code class="data-type">object</code></td>
+            <td align="left"><code className="data-type">object</code></td>
             <td align="left"><code>defaultPhrases</code></td>
             <td align="left"><code>...</code></td>
           </tr>
           <tr>
             <td align="left"><code>rightKeyPressed</code></td>
-            <td align="left"><code class="data-type">function</code></td>
+            <td align="left"><code className="data-type">function</code></td>
             <td align="left"><code>noop</code></td>
             <td align="left">Called when <code>right</code> key of the keyboard is pressed.</td>
           </tr>
           <tr>
             <td align="left"><code>show</code></td>
-            <td align="left"><code class="data-type">function</code></td>
+            <td align="left"><code className="data-type">function</code></td>
             <td align="left"><code>noop</code></td>
             <td align="left">Shows the modal when initialized.</td>
           </tr>
           <tr>
             <td align="left"><code>showThumbnails</code></td>
-            <td align="left"><code class="data-type">boolean</code></td>
+            <td align="left"><code className="data-type">boolean</code></td>
             <td align="left"><code>true</code></td>
             <td align="left">Whether the gallery should show thumbnails.</td>
           </tr>
           <tr>
             <td align="left"><code>keyboard</code></td>
-            <td align="left"><code class="data-type">boolean</code></td>
+            <td align="left"><code className="data-type">boolean</code></td>
             <td align="left"><code>true</code></td>
             <td align="left">Whether the gallery should react to keyboard events.</td>
           </tr>
           <tr>
             <td align="left"><code>wrap</code></td>
-            <td align="left"><code class="data-type">boolean</code></td>
+            <td align="left"><code className="data-type">boolean</code></td>
             <td align="left"><code>false</code></td>
             <td align="left">Whether the gallery should cycle continuously or have hard stops.</td>
           </tr>
           <tr>
             <td align="left"><code>opacity</code></td>
-            <td align="left"><code class="data-type">number</code></td>
+            <td align="left"><code className="data-type">number</code></td>
             <td align="left"><code>1</code></td>
             <td align="left">Sets the opacity level for the component.</td>
           </tr>
           <tr>
             <td align="left"><code>backgroundColor</code></td>
-            <td align="left"><code class="data-type">string</code></td>
+            <td align="left"><code className="data-type">string</code></td>
             <td align="left"><code>#000000</code></td>
             <td align="left">Sets the background color of the gallery component.</td>
           </tr>
           <tr>
             <td align="left"><code>zIndex</code></td>
-            <td align="left"><code class="data-type">number</code></td>
+            <td align="left"><code className="data-type">number</code></td>
             <td align="left"><code>2000</code></td>
             <td align="left">Specifies the stack order of the component.</td>
           </tr>
@@ -148,31 +148,31 @@ const Options = () => (
         <tbody>
           <tr>
             <td align="left">photo</td>
-            <td align="left"><code class="data-type">string</code></td>
+            <td align="left"><code className="data-type">string</code></td>
             <td align="left"><code>undefined</code></td>
             <td align="left">The <code>src</code> attribute value of the image.</td>
           </tr>
           <tr>
             <td align="left">number</td>
-            <td align="left"><code class="data-type">number</code></td>
+            <td align="left"><code className="data-type">number</code></td>
             <td align="left"><code>undefined</code></td>
             <td align="left">The current number of the photo.</td>
           </tr>
           <tr>
             <td align="left">caption</td>
-            <td align="left"><code class="data-type">string</code></td>
+            <td align="left"><code className="data-type">string</code></td>
             <td align="left"><code>undefined</code></td>
             <td align="left">Photo description.</td>
           </tr>
           <tr>
             <td align="left">subcaption</td>
-            <td align="left"><code class="data-type">string</code></td>
+            <td align="left"><code className="data-type">string</code></td>
             <td align="left"><code>undefined</code></td>
             <td align="left">Photo secondary description, like the photo author or the name of the place where it was taken.</td>
           </tr>
           <tr>
             <td align="left">thumbnail</td>
-            <td align="left"><code class="data-type">string</code></td>
+            <td align="left"><code className="data-type">string</code></td>
             <td align="left"><code>undefined</code></td>
             <td align="left">The <code>url</code> of the photo thumbnail. The preferred size for each thumbnail is <code>100x67</code>.</td>
           </tr>
