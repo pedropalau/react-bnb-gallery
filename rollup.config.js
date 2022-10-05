48e06165a03f031d718b63ebcc080a21016077b1
Pedro Enrique Palau Isaac <pepalauisaac@gmail.com>
2022-10-05 09:40:07 -0500

    starts updating to typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,54 +1,39 @@
-import babel from '@rollup/plugin-babel';
-import url from '@rollup/plugin-url';
-import commonjs from '@rollup/plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
-import resolve from 'rollup-plugin-node-resolve';
-import scss from 'rollup-plugin-scss';
-import stylelint from 'rollup-plugin-stylelint';
-import svgr from '@svgr/rollup';
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
 
-import pkg from './package.json';
-
-const createConfig = (output) => ({
-  input: 'src/index.js',
-  output: Object.assign.call({
-    sourcemap: true,
-  }, output),
-  plugins: [
-    external({
-      includeDependencies: true,
-    }),
-    scss({
-      modules: true,
-      sourcemap: false,
-      output: 'dist/style.css',
-    }),
-    url(),
-    svgr(),
-    babel({
-      babelHelpers: 'runtime',
-      exclude: 'node_modules/**',
-    }),
-    resolve(),
-    commonjs(),
-    stylelint({
-      fix: false,
-      include: ['src/scss/**.scss'],
-      syntax: 'scss',
-      quiet: false,
-    }),
-  ],
-});
+import pkg from "./package.json";
 
 export default [
-  createConfig({
-    file: pkg.main,
-    format: 'cjs',
-    exports: 'named',
-  }),
-  createConfig({
-    file: pkg.module,
-    format: 'esm',
-    exports: 'named',
-  }),
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+    ],
+    external: ["react", "react-dom"],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
 ];
