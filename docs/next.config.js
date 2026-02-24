1e6847b731e12758db75923fc9b5c710ad49171c
Pedro Palau <12257885+pedropalau@users.noreply.github.com>
2026-02-24 18:12:01 -0500

    docs: upgrade unified docs app to next 16.1.6

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['react-bnb-gallery'],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 module.exports = nextConfig;
